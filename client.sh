9ef688d1e85ce24c5841855229affb4dbbb5c681
ericg <ergare.17@gmail.com>
2025-12-18 23:00:16 -0600

    Fixed bug

--- client.sh
+++ client.sh
@@ -1,7 +1,6 @@
 echo "== DAM-VIOD Xat =="
 read -p "IP servidor: " server_ip
 read -p "Port servidor: " server_port
-
 read -p "Nom d'usuari: " username
 
 # Quan es prem Ctrl + C, el programa envia BYE al servidor
@@ -11,16 +10,15 @@ trap 'echo "BYE $username" | nc -q 0 $server_ip $server_port 2>/dev/null; exit 0
 echo "HELLO $username" | nc -q 0 $server_ip $server_port
 
 
-echo -e "\n== Benvingut! =="
-echo -e "\q per sortir\n"
+echo -e "\n\q per sortir"
+echo "== Benvingut! =="
 
 while true
 do
-	read -p "($username): " message
+	read -r -p "($username): " message
 
-	echo "$message"
 	# Si l'input és \q, s'envia un missatge BYE al servidor i es tanca el programa
-	if [ "$message" = "\q" ]; then
+	if [ "$message" = "\\q" ]; then
 		echo "BYE $username" | nc -q 0 $server_ip $server_port
 		break
 	fi
