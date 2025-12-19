2cf87d98b871d752d9e20a614f7b7a435f4d0baa
ericg <ergare.17@gmail.com>
2025-12-19 05:44:29 +0100

    Millora del server.sh

--- server.sh
+++ server.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-current_ip=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
+server_ip=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
 port=5555
 
 echo "== DAM-VIOD Xat =="
-echo "IP: $current_ip"
+echo "IP: $server_ip"
 echo "Port: $port"
-
-echo -e "\nEscriu en el teu client:"
-echo "echo \"missatge\" | nc $current_ip $port"
-echo -e "\nCtrl + C per sortir\n"
 
 echo -e "== Missatges rebuts =="
 
 while true
 do
-	# El programa escolta el següent missatge
-	nc -l 5555
+	# -l escolta el port, -v mostra més missatge, -n mostra la direcció en format IP
+	output=$(nc -l -v -n 5555 2>&1)
+
+	# Extreu la IP de la línia "Connection received ..." i el 4t paràmetre
+	client_ip=$(echo "$output" | grep "Connection received" | cut -d " " -f 4)
+	# Es guarda a $message, l'úlltima lÍnia d'$output
+	message=$(echo "$output" | tail -n1)
+
+	header=$(echo "$message" | cut -d " " -f 1)
+	user=$(echo "$message" | cut -d " " -f 2)
+
+	case "$header" in
+	    HELLO)
+	        echo "--> ${user}@${client_ip} s'ha unit al xat"
+	        ;;
+	    MESSAGE)
+		input_user=$(echo "$message" | cut -d " " -f 3)
+
+		echo "($user): $input_user"
+	        ;;
+	    BYE)
+	        echo "--> ${user}@${client_ip} ha abandonat el xat"
+	        ;;
+	    *)
+	        echo "Capçalera desconeguda"
+	        ;;
+	esac
+
 done
